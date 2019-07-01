5a90e0bff1d6492565a7bcf658185d5e69fecf4d
David Zhang <davidzhang501@gmail.com>
2019-06-30 21:33:55 -0700

    Added license headers

--- app/src/main/java/com/lunchareas/echomp/utils/NavUtils.java
+++ app/src/main/java/com/lunchareas/echomp/utils/NavUtils.java
@@ -1,3 +1,20 @@
+/*
+Echo Music Player
+Copyright (C) 2019 David Zhang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lunchareas.echomp.utils;
 
 
@@ -17,6 +34,7 @@ import com.lunchareas.echomp.models.Album;
 import com.lunchareas.echomp.models.Artist;
 import com.lunchareas.echomp.models.Playlist;
 import com.lunchareas.echomp.models.Song;
+
 
 public class NavUtils {
 
--- app/src/main/java/com/lunchareas/echomp/widgets/VerticalSeekBar.java
+++ app/src/main/java/com/lunchareas/echomp/widgets/VerticalSeekBar.java
@@ -1,10 +1,29 @@
+/*
+Echo Music Player
+Copyright (C) 2019 David Zhang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lunchareas.echomp.widgets;
+
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.SeekBar;
+
 
 public class VerticalSeekBar extends SeekBar {
 
